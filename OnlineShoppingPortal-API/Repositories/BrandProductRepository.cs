fe4c960c6c7cd23198a501802b5653d32cace9e1
AuntPyoneMaung <za7196@gmail.com>
2023-02-01 13:43:48 +0800

    comments on repository implementation added

--- OnlineShoppingPortal-API/Repositories/BrandProductRepository.cs
+++ OnlineShoppingPortal-API/Repositories/BrandProductRepository.cs
@@ -1,5 +1,25 @@
 ﻿namespace OnlineShoppingPortal_API.Repositories
 {
+    /*
+     * 
+     * repository pattern is for data access abstraction from the
+     * rest of the application in handling database communication. 
+     * 
+     * A consistent interface for working with data, no matter what 
+     * data storage platform used. Like a universal adapater between
+     * application and data.
+     * 
+     * It encapsulates the data access logic so it makes testing, 
+     * maintaining and changing the logic easier. 
+     * 
+     * ***
+     * As this project is small and does not anticipate major structural
+     * changes, with fixed data access, the repository patterns are not 
+     * implemented fully for all models in this project. A few repositories
+     * are implemented to showcase its capability. 
+     * ***
+     * 
+     * */
     public class BrandProductRepository
     {
     }
